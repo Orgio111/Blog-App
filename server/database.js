325caf2461a342ad7f4a6447c42e4da1636f3f98
MrOrgio <oorgio340@gmail.com>
2025-02-21 15:24:39 +0800

    edit

--- server/database.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require("sequelize");
-
-const sequelize = new Sequelize("blog_db", "root", "Orgio1295", {
-    host: "localhost",
-    dialect: "mysql",
-});
-
-module.exports = sequelize;
